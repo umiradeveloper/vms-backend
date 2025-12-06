f68c250a69ada2593b0f20ea1b46b874f570b5e8
hafid hanafi <hafidhanafid@gmail.com>
2025-12-06 15:42:44 +0800

    database cost control

---
+++ menus_costcontrol.sql
@@ -0,0 +1,6 @@
+INSERT INTO vms.menu (id_menu,code_apps,code_menu,nama_menu,menu_title,icon_menu,type_menu,active_menu,selected_menu,dircange_menu,path_menu) VALUES
+	('8d20da00-21bb-4496-86d1-c106c6ac93b9','73fd69aa-2f3d-4ded-babd-022adc5a2f99','01|02','Cost Control',NULL,'ti-briefcase','sub',NULL,'false','false',''),
+	 ('fb93383b-9de9-4b26-baba-fc0470898ba2','73fd69aa-2f3d-4ded-babd-022adc5a2f99','01|02|01','Proyek',NULL,'ti-user','link',NULL,'false','false','/apps/CostControl/Proyek/CreateProyek'),
+	 ('fb907101-0ae5-4590-8410-b38f801fa3b1','73fd69aa-2f3d-4ded-babd-022adc5a2f99','01|02|02','Daftar Proyek',NULL,'ti-user','link',NULL,'false','false','/apps/CostControl/Proyek/DaftarProyek'),
+	 ('ac7da452-4efc-4601-9c4a-03f631321662','73fd69aa-2f3d-4ded-babd-022adc5a2f99','01|02|03','RAPA',NULL,'ti-user','link',NULL,'false','false','/apps/CostControl/Rapa/DaftarRapa'),
+	 ('f28e1bd2-6d1d-46a7-9070-2627b5462984','73fd69aa-2f3d-4ded-babd-022adc5a2f99','01|02|04','Biaya Kontruksi',NULL,'ti-user','link',NULL,'false','false','/apps/CostControl/BiayaKontruksi/DaftarProyekBk');
