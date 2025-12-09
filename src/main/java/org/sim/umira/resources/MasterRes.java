f667b376f9f3739ec84801f05f4599a0ffa4f66a
hafid hanafi <hafidhanafid@gmail.com>
2025-12-09 14:36:08 +0800

    master week

--- src/main/java/org/sim/umira/resources/MasterRes.java
+++ src/main/java/org/sim/umira/resources/MasterRes.java
@@ -1,15 +1,23 @@
 package org.sim.umira.resources;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.IsoFields;
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import org.sim.umira.entities.BranchEntity;
 import org.sim.umira.entities.RoleEntity;
+import org.sim.umira.entities.CostControl.ProyekEntity;
 import org.sim.umira.handlers.ResponseHandler;
 
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -31,4 +39,39 @@ public class MasterRes {
         return Response.ok().entity(ResponseHandler.ok("Inquiry Role Success", be)).build();
     }
     
+    @GET
+    @Path("/get-week-by-project")
+    public Response getWeekByProject(
+        @QueryParam("id_project") String id_project
+    ){
+        try {
+            ProyekEntity pe = ProyekEntity.findById(id_project);
+            List<WeekData> result = new ArrayList<>();
+            LocalDate start = pe.tanggal_awal_kontrak;
+            LocalDate end = pe.tanggal_akhir_kontrak;
+            LocalDate currentStart = start;
+            int weekNumber = 1;
+
+        while (!currentStart.isAfter(end)) {
+            LocalDate currentEnd = currentStart.plusDays(6);
+            if (currentEnd.isAfter(end)) {
+                currentEnd = end;
+            }
+
+            result.add(new WeekData(weekNumber, currentStart, currentEnd));
+
+            weekNumber++;
+            currentStart = currentStart.plusDays(7);
+        }
+
+            return Response.ok().entity(ResponseHandler.ok("Inquiry Role Success", result.stream().sorted(Comparator.comparing(WeekData::week)).toList())).build();
+        } catch (Exception e) {
+            throw new InternalError(e.getMessage());
+            // TODO: handle exception
+        }
+        
+    }
+
+    public record WeekData(int week, LocalDate startDate, LocalDate endDate) {}
+
 }
