69a5f62a8acac6a90e18179e5efcee163347da06
hafid hanafi <hafidhanafid@gmail.com>
2026-01-17 14:44:59 +0700

    validasi

--- src/main/java/org/sim/umira/resources/CostControl/RapaRes.java
+++ src/main/java/org/sim/umira/resources/CostControl/RapaRes.java
@@ -20,6 +20,7 @@ import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import jakarta.ws.rs.BadRequestException;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.NotFoundException;
@@ -79,9 +80,10 @@ public class RapaRes {
                 String uuid = java.util.UUID.randomUUID().toString();
                 final int idx = i;
                 // System.out.println(create.kategori.get(idx));
-                // SatuanEntity satuan = SatuanEntity.find("nama_satuan = ?1", create.satuan.get(idx)).firstResult();
-                // KategoriEntity kategori = KategoriEntity.find("nama_kategori = ?1", create.kategori.get(idx)).firstResult();
-                // if(satuan != null && kategori != null){
+                SatuanEntity satuan = SatuanEntity.find("nama_satuan = ?1", create.satuan.get(idx)).firstResult();
+                KategoriEntity kategori = KategoriEntity.find("nama_kategori = ?1", create.kategori.get(idx)).firstResult();
+                
+                if(satuan != null && kategori != null){
                     session.doWork(connection -> {
                         try (PreparedStatement ps = connection.prepareStatement(
                             "INSERT INTO cc_rapa (id_rapa, id_proyek, kategori, kode_rap, `group`, item_pekerjaan, spesifikasi, satuan, volume, harga_satuan, harga_total) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
@@ -108,9 +110,9 @@ public class RapaRes {
                         session.flush();
                         session.clear();
                     }
-                // }else{
-                //     throw new NotFoundException("Nama Satuan atau nama kategori tidak tersedia");
-                // }
+                }else{
+                    throw new BadRequestException("Nama Satuan atau nama kategori tidak tersedia");
+                }
                 
             }
             
